805446dbcbd9bdd14ed9ef993c1f8a6e5e356bff
arima0714 <32933855+arima0714@users.noreply.github.com>
2020-11-26 12:34:19 +0900

    ベンチマーク実行スクリプトのファイル名と挙動が一致するようにした

--- exec_benchmark_excludeBTSP.sh
+++ exec_benchmark_excludeBTSP.sh
@@ -1,7 +1,7 @@
 # ジョブスクリプトを作成し、ベンチマークを実行するスクリプト
 
 # 基本的な配列・変数の宣言
-benchmarks=("bt" "cg" "dt" "ep" "ft" "is" "lu" "mg" "sp")
+benchmarks=("cg" "dt" "ep" "ft" "is" "lu" "mg")
 bin_dir=${PWD}"/"
 classes=("S" "W" "A" "B" "C" "D" "E" "F")
 classes=("A" "B" "C" "D")
