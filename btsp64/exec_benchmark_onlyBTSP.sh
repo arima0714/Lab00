8660daf6c60ea6e6773cb52df6a35742e7ba584a
arima0714 <32933855+arima0714@users.noreply.github.com>
2020-12-10 15:32:26 +0900

    64並列で実行する準備をした

--- btsp64/exec_benchmark_onlyBTSP.sh
+++ btsp64/exec_benchmark_onlyBTSP.sh
@@ -5,7 +5,7 @@ benchmarks=("bt" "sp")
 bin_dir=${PWD}"/"
 classes=("S" "W" "A" "B" "C" "D" "E" "F")
 classes=("A" "B" "C" "D")
-processes=("25")
+processes=("64")
 
 for benchmark in ${benchmarks[@]}
 do
