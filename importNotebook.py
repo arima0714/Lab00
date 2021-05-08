4053b9a2e734f2cdace53bb4e52e8f7370e9baa6
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-05-08 12:08:54 +0900

    WIP

--- importNotebook.py
+++ importNotebook.py
@@ -3,3 +3,21 @@ from IPython import get_ipython
 from nbformat import read
 from IPython.core.interactiveshell import InteractiveShell
 
+def find_notebook(fullname, path=None):
+    name = fullname.rsplit('.', 1)[-1]
+    if not path:
+        path = ['']
+    for d in path:
+        nb_path = os.path.join(d, name + ".ipynb")
+        if os.path.isfile(nb_path):
+            return nb_path
+        nb_path = nb_path.replace("_", " ")
+        if os.path.isfile(nb_path)
+            return nb_path
+
+class NotebookLoader(object):
+    def __init__(self, path=None):
+        self.shell = InteractiveShell.instance()
+        self.path = path
+
+    def load_module(self, fullname):
