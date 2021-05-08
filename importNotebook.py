f86956112da56d52171f537f9d15bdab1cbc26f7
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-05-08 13:14:38 +0900

    未実行だがノートをインポートするライブラリが完成

--- importNotebook.py
+++ importNotebook.py
@@ -21,3 +21,37 @@ class NotebookLoader(object):
         self.path = path
 
     def load_module(self, fullname):
+        path = find_notebook(fullname, self.path)
+        with io.open(path, 'r', encoding='utf-8') as f:
+            nb = read(f, 4)
+        mod = types.ModuleType(fullname)
+        mod.__file__ = path
+        mod.__loader__ = self
+        mod.__dict__['get_ipython'] = get_ipython
+        sys.modules[fullname] = mod
+        save_user_ns = self.shell.user_ns
+        self.shell.user_ns = mod.__dict__
+        try:
+            for cell in nb.cells:
+                if cell.cell_type == 'code':
+                    code = self.shell.input_transformer_manager.transform_cell(cell.source)
+                    exec(code, mod.__dict__)
+        finally:
+            self.shell.user_ns = save_user_ns
+        return mod
+
+
+    class NotebookFinder(object):
+        def __init__(self):
+            self.loaders = {}
+
+        def find_module(selfself, fullname, path=None):
+            nb_path = find_notebook(fullname, path)
+            if not nb_path:
+                return
+            key = path
+            if path:
+                key = os.path.sep.join(path)
+            if key not in self.loaders:
+                self.loaders[key] = NotebookLoader(path)
+            return self.loaders[key]
