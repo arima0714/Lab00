4f1b582da674973c58c3ad25c45f7d3d4bf824c9
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-05-08 17:07:22 +0900

    実データのプロットまでできた

--- app.py
+++ app.py
@@ -69,3 +69,27 @@ if(fixedTarget == "コア数"):
     raw_x = lib.ConvertBenchmarkClasses(raw_x)
 
 # グラフのプロット
+## 準備
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import numpy as np
+fig = plt.figure(figsize=(12, 9))
+ax = plt.axes()
+## 実データをsklearn用に変形
+raw_x = np.array(raw_x).reshape(-1,1)
+raw_y = np.array(raw_y).reshape(-1, 1)
+## 説明変数と目的変数に分割
+notTrain_x, train_x, target_x = raw_x[0], raw_x[1:-1], raw_x[-1]
+notTrain_y, train_y, target_y = raw_y[0], raw_y[1:-1], raw_y[-1]
+## 実データをそれぞれプロット
+plt.scatter(train_x, train_y, marker="o", label="予測に用いた関数コール回数")
+plt.scatter(target_x, target_y, marker="o", label="予測したい関数コール回数の実測値")
+plt.scatter(notTrain_x, notTrain_y, marker="o", label="予測に用いなかった関数コール回数")
+## モデル式をプロットするために変数”plot_x”を用意する
+plot_x = np.linspace(0.01, 256, 500)
+plot_x = np.array(plot_x).reshape(-1, 1)
+
+
+plt.xlabel("Cores or Problem size")
+plt.legend()
+st.pyplot(fig)
