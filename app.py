1f92c4f27f0a290923f8970cc2c62a3e46e7d2cf
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-05-08 14:44:47 +0900

    ライブラリノートを自動的に実行可能な形式にするスクリプトを追加した

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import streamlit as st
+import subprocess
 import pandas as pd
 
 # DataFrameを生成するcache機能付の関数を定義
 @st.cache
 def get_df(s):
     return pd.DataFrame({'id':['1','2','3'], 'name':['X', 'Y', s]})
+
+@st.cache
+def gen_lib():
+    subprocess.run(["jupyter", "nbconvert", "--to", "python", "lib.ipynb"])
+# ライブラリノートを実行可能な形式に変換
+gen_lib()
 
 # text formを生成
 s = st.text_input('input s')
