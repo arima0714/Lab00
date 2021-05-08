327ced35d4334c851ef5a074e84e47b5bd136d9d
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-05-08 16:29:03 +0900

    関数名を指定するところまでできた

--- app.py
+++ app.py
@@ -7,26 +7,26 @@ import pandas as pd
 def get_df(s):
     return pd.DataFrame({'id':['1','2','3'], 'name':['X', 'Y', s]})
 
-@st.cache
 def gen_lib():
     subprocess.run(["jupyter", "nbconvert", "--to", "python", "lib.ipynb"])
+    subprocess.run(["mv", "lib.py", "libLab00.py"])
 # ライブラリノートを実行可能な形式に変換
 gen_lib()
 # 変換されたライブラリノートをimport
-import lib
+import libLab00 as lib
 
-# text formを生成
-s = st.text_input('input s')
-df = get_df(s)
-
-# DataFrameをテーブルで表示
-st.markdown('# Table')
-st.table(df)
-
-# histgramを表示
-st.markdown('# Histgram')
-df['name'].hist()
-st.pyplot()
+# # text formを生成
+# s = st.text_input('input s')
+# df = get_df(s)
+#
+# # DataFrameをテーブルで表示
+# st.markdown('# Table')
+# st.table(df)
+#
+# # histgramを表示
+# st.markdown('# Histgram')
+# df['name'].hist()
+# st.pyplot()
 
 # コア数と問題サイズのどちらを固定するかを選択
 fixedTarget = st.selectbox("コア数と問題サイズのどちらを固定するか？", ["コア数", "問題サイズ"])
@@ -35,3 +35,27 @@ st.write(f"{fixedTarget} を選択")
 # ベンチマークを選択
 benchmark = st.selectbox("ベンチマークを選択", lib.benchmarks)
 st.write(f"{benchmark} を選択")
+
+
+if benchmark == "bt" or benchmark == "sp":
+    processes = lib.processes_onlyBTSP
+else:
+    processes = lib.processes_excludeBTSP
+benchmarkClasses = lib.benchmarkClasses
+
+if fixedTarget == "問題サイズ":
+    fixed = st.selectbox("どの問題サイズで固定するか？", lib.benchmarkClasses)
+else:
+    fixed = st.selectbox("どのコア数で固定するか？", processes)
+st.write(f"{fixed}を選択")
+
+if fixedTarget == "問題サイズ":
+    targetRawDF = lib.returnRawDFperBenchmark(Benchmark=benchmark, fix="Class", Processes=processes, FixedBenchmarkClass=fixed)
+else:
+    targetRawDF = lib.returnRawDFperBenchmark(Benchmark=benchmark, fix="Process", benchmarkClass=benchmarkClasses)
+
+st.markdown('# targetRawDF')
+st.table(targetRawDF)
+
+functionNames = targetRawDF.index.tolist()
+functionName = st.selectbox("関数名を選択", functionNames)
