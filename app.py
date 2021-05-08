ee22017f04e40a51ca2624775c754bb153dd1690
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-05-08 14:40:35 +0900

    サンプルが動くことを確認

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+# DataFrameを生成するcache機能付の関数を定義
+@st.cache
+def get_df(s):
+    return pd.DataFrame({'id':['1','2','3'], 'name':['X', 'Y', s]})
+
+# text formを生成
+s = st.text_input('input s')
+df = get_df(s)
+
+# DataFrameをテーブルで表示
+st.markdown('# Table')
+st.table(df)
+
+# histgramを表示
+st.markdown('# Histgram')
+df['name'].hist()
+st.pyplot()
