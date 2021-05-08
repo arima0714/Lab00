cb4403bf760fd143985570f8dace423d4831f925
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-05-08 19:24:57 +0900

    新規でプロットした最初のデータを除外したモデル式・ロバスト回帰によるモデル式をひょうじするようにした

--- app.py
+++ app.py
@@ -84,13 +84,51 @@ notTrain_y, train_y, target_y = raw_y[0], raw_y[1:-1], raw_y[-1]
 ## 実データをそれぞれプロット
 plt.scatter(train_x, train_y, marker="o", label="予測に用いた関数コール回数")
 plt.scatter(target_x, target_y, marker="o", label="予測したい関数コール回数の実測値")
-plt.scatter(notTrain_x, notTrain_y, marker="o", label="予測に用いなかった関数コール回数")
+plt.scatter(notTrain_x, notTrain_y, marker="o", label="最初のデータを除外した時に予測に用いなかった関数コール回数")
 ## モデル式をプロットするために変数”plot_x”を用意する
 plot_x = np.linspace(0.01, 256, 500)
 plot_x = np.array(plot_x).reshape(-1, 1)
+## 最初のデータを除外したモデル式のプロット
+if st.checkbox("線形モデル(最初のデータを除外)"):
+    model_lin = lib.ModelLin(train_x, train_y, benchmark, functionName, test_ratio=0)
+    model_lin.calc_lr()
+    plot_y_lin = model_lin.predict(plot_x)
+    plt.plot(plot_x, plot_y_lin, label="線形モデル(最初のデータを除外)")
+if st.checkbox("対数モデル(最初のデータを除外)"):
+    model_log10 = lib.ModelLog10(train_x, train_y, benchmark, functionName, test_ratio=0)
+    model_log10.calc_lr()
+    plot_y_log10 = model_log10.predict(plot_x)
+    plt.plot(plot_x, plot_y_log10, label="対数モデル(最初のデータを除外)")
+if st.checkbox("反比例モデル(最初のデータを除外)"):
+    model_ip = lib.ModelIP(train_x, train_y, benchmark, functionName, test_ratio=0)
+    model_ip.calc_lr()
+    plot_y_ip = model_ip.predict(plot_x)
+    plt.plot(plot_x, plot_y_ip, label="反比例モデル(最初のデータを除外)")
+if st.checkbox("線形飽和モデル(最初のデータを除外)"):
+    model_branch = lib.ModelBranch(train_x, train_y, benchmark, functionName, test_ratio=0)
+    model_branch.calc_lr()
+    plot_y_branch = model_branch.predict(plot_x)
+    plt.plot(plot_x, plot_y_branch, label="線形飽和モデル(最初のデータを除外)")
 
-if st.checkbox("モデル名"):
-    pass
+# 最初のデータを含む学習データと最後のデータのみの試験用データに分割
+train_x, target_x = raw_x[:-1], raw_x[-1]
+train_y, target_y = raw_y[:-1], raw_y[-1]
+# ロバスト回帰によるモデル式のプロット
+if st.checkbox("線形モデル(ロバスト回帰)"):
+    model_lin_rob = lib.ModelLin_rob(train_x=train_x, train_y=train_y, target_x=target_x, target_y=target_y)
+    model_lin_rob.calc_hr()
+    plot_y_lin_rob = model_lin_rob.predict(plot_x)
+    plt.plot(plot_x, plot_y_lin_rob, label="線形モデル（ロバスト回帰）")
+if st.checkbox("反比例モデル(ロバスト回帰)"):
+    model_ip_rob = lib.ModelIp_rob(train_x=train_x, train_y=train_y, target_x=target_x, target_y=target_y)
+    model_ip_rob.calc_hr()
+    plot_y_ip_rob = model_ip_rob.predict(plot_x)
+    plt.plot(plot_x, plot_y_ip_rob, label="線形モデル（ロバスト回帰）")
+if st.checkbox("対数モデル(ロバスト回帰)"):
+    model_log_rob = lib.ModelLog10_rob(train_x=train_x, train_y=train_y, target_x=target_x, target_y=target_y)
+    model_log_rob.calc_hr()
+    plot_y_log_rob = model_log_rob.predict(plot_x)
+    plt.plot(plot_x, plot_y_log_rob, label="対数モデル（ロバスト回帰）")
 
 plt.xlabel("Cores or Problem size")
 plt.legend()
