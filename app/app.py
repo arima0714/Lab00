edd24b377b9b3eeeb1c525bc5aabb04ed246940d
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-08-27 10:45:15 +0900

    サイドバーの選択によって出力が変わるようにした

--- app/app.py
+++ app/app.py
@@ -2,13 +2,27 @@ import streamlit as st
 import subprocess
 import pandas as pd
 
-dimension = st.sidebar.selectbox('プロットする次元',('２次元', '３次元'))
 
-if dimension == '２次元':       # 2次元グラフの描画
-    pass
+@st.cache
+def gen_lib():
+    subprocess.run(["jupyter", "nbconvert", "--to", "python", "../lib/lib.ipynb"])
+    subprocess.run(["mv", "lib.py", "libLab00.py"])
 
-elif dimension == '３次元':     # 3次元グラフの描画
-    pass
+
+# ライブラリノートを実行可能な形式に変換
+gen_lib()
+# 変換されたライブラリノートをimport
+import libLab00 as lib
+
+dimension = st.sidebar.selectbox("プロットする次元", ("２次元", "３次元"))
+
+if dimension == "２次元":  # 2次元グラフの描画
+
+    st.markdown("# ２次元グラフのプロット")
+
+elif dimension == "３次元":  # 3次元グラフの描画
+
+    st.markdown("# ３次元グラフのプロット")
 
 else:
     pass
