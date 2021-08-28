2474533f6bb0b5f845aa17998e74b3dc0ac56cc1
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-08-28 10:15:19 +0900

    共通部分を共通なコード位置に移動した

--- app/app.py
+++ app/app.py
@@ -16,6 +16,29 @@ import libLab00 as lib
 
 dimension = st.sidebar.selectbox("プロットする次元", ("２次元", "３次元"))
 
+# 生データの取得
+benchmarkName = [st.selectbox(options=["cg", "ep", "ft", "is", "lu", "mg"], label="ベンチマーク名")]
+classes = ["A", "B", "C", "D"]
+processes = [2, 4, 8, 16, 32, 64, 128, 256]
+csvDirPath = "../csv_files/"
+rawDataDF = lib.returnCollectedExistingData(benchmarkNames=benchmarkName, classes=classes, processes=processes, csvDirPath=csvDirPath)
+# 問題サイズを数値化
+programSize = rawDataDF['benchmarkClass'].tolist()
+programSizeInNum = lib.convertBenchmarkClasses_problemSizeInNPB(inputList=programSize)
+rawDataDF["benchmarkClassInNum"] = programSizeInNum
+# プロット用のDFを作成
+functionNames = sorted(list(set(rawDataDF['functionName'].tolist())))
+functionName = st.selectbox(options=functionNames, label='関数名')
+DFperFunctionName = rawDataDF[rawDataDF["functionName"]==functionName]
+numCore = DFperFunctionName["process"].tolist()
+programSize = DFperFunctionName["benchmarkClassInNum"].tolist()
+functionCallCount = DFperFunctionName["functionCallNum"].tolist()
+DFtoPlot = pd.DataFrame({"問題サイズ":programSize, "コア数":numCore, "関数コール回数":functionCallCount})
+# プロット
+enableLogX = st.checkbox(label="問題サイズの軸の対数化")
+enableLogY = st.checkbox(label='コア数の軸の対数化')
+enableLogZ = st.checkbox(label='関数コール回数の軸の対数化')
+
 if dimension == "２次元":  # 2次元グラフの描画
 
     st.markdown("# ２次元グラフのプロット")
@@ -26,28 +49,6 @@ elif dimension == "３次元":  # 3次元グラフの描画
 
     st.markdown("# ３次元グラフのプロット")
 
-    # 生データの取得
-    benchmarkName = [st.selectbox(options=["cg", "ep", "ft", "is", "lu", "mg"], label="ベンチマーク名")]
-    classes = ["A", "B", "C", "D"]
-    processes = [2, 4, 8, 16, 32, 64, 128, 256]
-    csvDirPath = "../csv_files/"
-    rawDataDF = lib.returnCollectedExistingData(benchmarkNames=benchmarkName, classes=classes, processes=processes, csvDirPath=csvDirPath)
-    # 問題サイズを数値化
-    programSize = rawDataDF['benchmarkClass'].tolist()
-    programSizeInNum = lib.convertBenchmarkClasses_problemSizeInNPB(inputList=programSize)
-    rawDataDF["benchmarkClassInNum"] = programSizeInNum
-    # プロット用のDFを作成
-    functionNames = sorted(list(set(rawDataDF['functionName'].tolist())))
-    functionName = st.selectbox(options=functionNames, label='関数名')
-    DFperFunctionName = rawDataDF[rawDataDF["functionName"]==functionName]
-    numCore = DFperFunctionName["process"].tolist()
-    programSize = DFperFunctionName["benchmarkClassInNum"].tolist()
-    functionCallCount = DFperFunctionName["functionCallNum"].tolist()
-    DFtoPlot = pd.DataFrame({"問題サイズ":programSize, "コア数":numCore, "関数コール回数":functionCallCount})
-    # プロット
-    enableLogX = st.checkbox(label="問題サイズの軸の対数化")
-    enableLogY = st.checkbox(label='コア数の軸の対数化')
-    enableLogZ = st.checkbox(label='関数コール回数の軸の対数化')
     plotType = st.selectbox(options=["scatter", "mesh"], label='プロットするタイプの選択')
     if plotType == "scatter":
         fig = px.scatter_3d(DFtoPlot, x='問題サイズ', y='コア数', z='関数コール回数', log_x=enableLogX, log_y=enableLogY, log_z=enableLogZ)
