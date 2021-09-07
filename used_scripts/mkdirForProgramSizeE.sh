47676c71344722839c482d0ec353bddfc89687ee
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-07 17:15:07 +0900

    作成予定のディレクトリを変数に格納するようにした

--- used_scripts/mkdirForProgramSizeE.sh
+++ used_scripts/mkdirForProgramSizeE.sh
@@ -1,10 +1,13 @@
 numOfCores=(32 64 128 256)
 benchmarkNames=(bt sp lu is ep cg mg ft)
+problemSize=E
 for numOfCore in "${numOfCores[@]}"
 do
     for benchmarkName in "${benchmarkNames[@]}"
     do
-        echo "numOfCore="$numOfCore
-        echo "benchmarkNames="$benchmarkName
+        dirName=./$problemSize/$numOfCore/$benchmarkName
+        echo $dirName
     done
 done
+
+# 使い方：`bin/`で実行する
