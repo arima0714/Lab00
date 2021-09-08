22db6e5ca399a9acbe0bb4adbe24272ece59dc64
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-08 14:21:33 +0900

    説明を追加した。TODOを追加した

--- used_scripts/genJobScriptForProgramSizeE.sh
+++ used_scripts/genJobScriptForProgramSizeE.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# 使い方：
-# 実行される処理：`bin/<コア数>/<問題サイズ(E)>/<ベンチマーク名>/<バイナリ>`のようにバイナリを設置する
+# 使い方：`bin/`で実行する。
+# 実行される処理：ジョブスクリプトを作成する。
 
 numOfCores=(32 64 128 256)
 benchmarkNames=(bt sp lu is ep cg mg ft)
 problemSize=E
 
-<executeEnvironment>
-<numOfExecuteEnvironment>
-<executingHour>
-<executeShellScript>
-<numOfCore>
-<processPerNode>
+# <executeEnvironment>
+# <numOfExecuteEnvironment>
+# <executingHour>
+# <executeShellScript>
+# <numOfCore>
+# <processPerNode>
 
 for numOfCore in "${numOfCores[@]}"
 do
     dirName=./$problemSize/$numOfCore/$benchmarkName/
     binName=$benchmarkName.$problemSize.x
-    cp $binName $dirName
+    # 各コア数ごとに異なる処理を行う
 done
