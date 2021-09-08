aee119cbe973e1a8b76276610fe5bb2fafe67781
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-08 15:23:05 +0900

    各コア数ごとの条件分岐で異なる値を同じ変数に入れるようにした

--- used_scripts/genJobScriptForProgramSizeE.sh
+++ used_scripts/genJobScriptForProgramSizeE.sh
@@ -13,38 +13,58 @@ do
         # 32コア
         echo "numOfCore=$numOfCore(32)"
         # <executeEnvironment>
+        executeEnvironment="q_core"
         # <numOfExecuteEnvironment>
+        numOfExecuteEnvironment=8
         # <executingHour>
+        executingHour=23
         # <executeShellScript>
         # <numOfCore>
+        numOfCore=32
         # <processPerNode>
+        processPerNode=4
     elif [ 64 -eq "$numOfCore" ]; then
         # 64コア
         echo "numOfCore=$numOfCore(64)"
         # <executeEnvironment>
+        executeEnvironment="q_core"
         # <numOfExecuteEnvironment>
+        numOfExecuteEnvironment=16
         # <executingHour>
+        executingHour=14
         # <executeShellScript>
         # <numOfCore>
+        numOfCore=64
         # <processPerNode>
+        processPerNode=4
     elif [ 128 -eq "$numOfCore" ]; then
         # 128コア
         echo "numOfCore=$numOfCore(128)"
         # <executeEnvironment>
+        executeEnvironment="f_node"
         # <numOfExecuteEnvironment>
+        numOfExecuteEnvironment=5
         # <executingHour>
+        executingHour=8
         # <executeShellScript>
         # <numOfCore>
+        numOfCore=128
         # <processPerNode>
+        processPerNode=28
     elif [ 256 -eq "$numOfCore" ]; then
         # 256コア
         echo "numOfCore=$numOfCore(256)"
         # <executeEnvironment>
+        executeEnvironment="f_node"
         # <numOfExecuteEnvironment>
+        numOfExecuteEnvironment=10
         # <executingHour>
+        executingHour=5
         # <executeShellScript>
         # <numOfCore>
+        numOfCore=256
         # <processPerNode>
+        processPerNode=28
     else
         echo "There is an inappropriate element."
     fi
