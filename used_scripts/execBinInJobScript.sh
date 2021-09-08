9c92fbb4c62e59ecf632f80cc9edd0bb3f9fc85a
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-08 10:26:28 +0900

    TAU関連の設定を追加した

--- used_scripts/execBinInJobScript.sh
+++ used_scripts/execBinInJobScript.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 numOfCore=$1
+numOfCoreInNode=$2
 benchmarkNames=(lu is ep cg mg ft)
 problemSize=E
 
 baseDir=$PWD
 echo $baseDir
 echo 'numOfCore='$numOfCore
+
+export PATH=\"\$PATH:/home/9/20IA1328/tau-2.29/x86_64/bin\"
+export TAU_MAKEFILE=/home/9/20IA1328/tau-2.29/x86_64/lib/Makefile.tau-papi-mpi-pdt
+export TAU_OPTIONS=-optRevert
+export PATH=\"\$PATH:/home/9/20IA1328/pdtoolkit-3.25.1/x86_64//bin\"
+export TAU_THROTTLE=0
+# 実行
+# ノードあたりAプロセスMPI全B プロセスを使用
+mpirun -npernode \$A -n \$B -x LD_LIBRARY_PATH \${BenchmarkFileName}
 
 for benchmarkName in "${benchmarkNames[@]}"
 do
