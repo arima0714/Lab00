b66f1bbd9db4458c8de9c5ddd26428a9992f03d3
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-08 09:56:27 +0900

    実行バイナリが存在する場合はバイナリ名を出力するようにした

--- used_scripts/execBinInJobScript.sh
+++ used_scripts/execBinInJobScript.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
 numOfCore=$1
-benchmarkNames=(bt sp lu is ep cg mg ft)
-programSize=E
+benchmarkNames=(lu is ep cg mg ft)
+problemSize=E
 
 baseDir=$PWD
 echo $baseDir
 echo 'numOfCore='$numOfCore
+
+for benchmarkName in "${benchmarkNames[@]}"
+do
+    # ディレクトリを実行環境用のディレクトリに移動
+    dirName=./$problemSize/$numOfCore/$benchmarkName
+    cd $dirName
+    # ベンチマークバイナリを実行
+    binName=$benchmarkName.$problemSize.x
+    if [ -e $binName ]; then
+        echo $binName
+    fi
+    # 元のディレクトリに戻る
+    cd $baseDir
+done
