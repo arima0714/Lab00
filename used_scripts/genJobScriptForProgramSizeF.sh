77895cb77921db5b1eeafab60f0234f9a464f5fe
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-12 13:39:30 +0900

    問題サイズF用のジョブスクリプトを作成するスクリプトを作る

---
+++ used_scripts/genJobScriptForProgramSizeF.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# 使い方：`bin/used_scripts/`で実行する。
+# 実行される処理：ジョブスクリプトを作成する。
+
+numOfCores=(32 64 128 256)
+
+jobScriptBaseFileName="jobScriptForProblemSizeE"
+jobScriptBaseDir="./"
+programSize="E"
+
+for numOfCore in "${numOfCores[@]}"
+do
+    # 各コア数ごとにジョブスクリプトを作成する
+    jobScriptFileName=$jobScriptBaseFileName$numOfCore
+    # 保存するディレクトリを保持する変数
+    saveDir="../$programSize/$numOfCore/"
+    jobScriptPath=$saveDir$jobScriptFileName
+    cp $jobScriptBaseDir$jobScriptBaseFileName $jobScriptPath
+    # <executeShellScript>
+    executeShellScript="execBenchmarkExcludeBTSPonE.sh"
+    # 各コア数ごとに異なる処理を行う
+    if [ 32 -eq "$numOfCore" ]; then
+        # 32コア
+        echo "numOfCore=$numOfCore(32)"
+        # <executeEnvironment>
+        executeEnvironment="q_core"
+        # <numOfExecuteEnvironment>
+        numOfExecuteEnvironment=8
+        # <executingHour>
+        executingHour=23
+        # <numOfCore>
+        numOfCore=32
+        # <processPerNode>
+        processPerNode=4
+    elif [ 64 -eq "$numOfCore" ]; then
+        # 64コア
+        echo "numOfCore=$numOfCore(64)"
+        # <executeEnvironment>
+        executeEnvironment="q_core"
+        # <numOfExecuteEnvironment>
+        numOfExecuteEnvironment=16
+        # <executingHour>
+        executingHour=14
+        # <numOfCore>
+        numOfCore=64
+        # <processPerNode>
+        processPerNode=4
+    elif [ 128 -eq "$numOfCore" ]; then
+        # 128コア
+        echo "numOfCore=$numOfCore(128)"
+        # <executeEnvironment>
+        executeEnvironment="f_node"
+        # <numOfExecuteEnvironment>
+        numOfExecuteEnvironment=5
+        # <executingHour>
+        executingHour=8
+        # <numOfCore>
+        numOfCore=128
+        # <processPerNode>
+        processPerNode=28
+    elif [ 256 -eq "$numOfCore" ]; then
+        # 256コア
+        echo "numOfCore=$numOfCore(256)"
+        # <executeEnvironment>
+        executeEnvironment="f_node"
+        # <numOfExecuteEnvironment>
+        numOfExecuteEnvironment=10
+        # <executingHour>
+        executingHour=5
+        # <numOfCore>
+        numOfCore=256
+        # <processPerNode>
+        processPerNode=28
+    else
+        echo "There is an inappropriate element."
+    fi
+    # <executeEnvironment>
+    sed -i -e "s/<executeEnvironment>/$executeEnvironment/g" $jobScriptPath
+    # <numOfExecuteEnvironment>
+    sed -i -e "s/<numOfExecuteEnvironment>/$numOfExecuteEnvironment/g" $jobScriptPath
+    # <executingHour>
+    sed -i -e "s/<executingHour>/$executingHour/g" $jobScriptPath
+    # <executeShellScript>
+    sed -i -e "s/<executeShellScript>/$executeShellScript/g" $jobScriptPath
+    # <numOfCore>
+    sed -i -e "s/<numOfCore>/$numOfCore/g" $jobScriptPath
+    # <processPerNode>
+    sed -i -e "s/<processPerNode>/$processPerNode/g" $jobScriptPath
+
+done
