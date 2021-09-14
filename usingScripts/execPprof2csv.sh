da6aef66efa1558645ce7f89d1a9869dd17bf7de
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-14 10:20:45 +0900

    実行方法を修正した

--- usingScripts/execPprof2csv.sh
+++ usingScripts/execPprof2csv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 使用方法：`./bin/`で実行する。
+# 引数：実行するスクリプトのパス
+# 処理内容：`pprof -s`による出力をCSV化するスクリプトをまとめて実行するスクリプト
+
 programSizes=("E" "F")
 numOfCores=("32" "64" "128" "256")
 
@@ -8,5 +12,6 @@ do
     for numOfCore in "${numOfCores[@]}"
     do
         echo "programSize=$programSize, numOfCore=$numOfCore"
+        $1 "$programSize" "$numOfCore"
     done
 done
