4f4d72da48c734eeed10ffd2aa7ecaa3d8577304
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-14 10:11:46 +0900

    不要な出力をしないようにした

--- usingScripts/convertPprofToCSVexcludeBTSP.sh
+++ usingScripts/convertPprofToCSVexcludeBTSP.sh
@@ -4,7 +4,6 @@
 # 処理内容：カレントディレクトリに存在する実験データから、`pprof -s`コマンドで集計したテキストファイル`<pprof_s.txt>`を出力する
 
 function exec_pprof_s() {
-	echo "$PWD"
 	pprof -s > pprof_s.txt
 }
 
@@ -12,7 +11,6 @@ function exec_pprof_s() {
 # 処理内容：カレントディレクトリに存在する`exec_pprof_s()`の実行結果をCSVに変換するスクリプト
 
 function convert_pprof2csv() {
-	echo "$PWD"
 	pprof_s_FileName=pprof_s.txt
 	csv_FileName=pprof_s.csv
 	cat ${pprof_s_FileName} | sed -n '/mean/,$p'| sed -e 's/char /char_/g' | sed -e 's/void /void_/g' | sed -e 's/double /double_/g' | sed -e 's/int /int_/g' | sed -e 's/.TAU application/.TAU_application/g' | sed -e 's/, /_/g' | sed "4,5d" | sed "1,2d" | awk -v OFS=, '{print $7, $4}' > "${csv_FileName}"
@@ -34,5 +32,6 @@ do
 	cd "$dirPath" || exit
 	exec_pprof_s
 	convert_pprof2csv
+	echo "_____$benchmarkName @$programSize/$numOfCore finished._____"
 	cd "$baseDir" || exit
 done
