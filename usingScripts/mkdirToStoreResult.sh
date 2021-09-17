2b6ca4710a712a1513c39d8fedc2f9fb061538bf
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-09-17 16:38:01 +0900

    結果を保持するためのディレクトリを作成するシェルスクリプトを問題サイズによらないものにした

--- usingScripts/mkdirToStoreResult.sh
+++ usingScripts/mkdirToStoreResult.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# 使い方：`bin/`で実行する
+# 引数：問題サイズ（アルファベット1文字）
+# 実行される処理：`bin/<コア数>/<問題サイズ>/<ベンチマーク名>`の様な構成のディレクトリを作成する
+
 numOfCores=(32 64 128 256)
 benchmarkNames=(bt sp lu is ep cg mg ft)
-problemSize=F
+problemSize=$1
 for numOfCore in "${numOfCores[@]}"
 do
     for benchmarkName in "${benchmarkNames[@]}"
@@ -10,6 +15,3 @@ do
         mkdir -p $dirName
     done
 done
-
-# 使い方：`bin/`で実行する
-# 実行される処理：`bin/<コア数>/<問題サイズ(E)>/<ベンチマーク名>`のような構成のディレクトリを作成する
