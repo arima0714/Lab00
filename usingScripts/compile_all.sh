b7bc298ea9d00f8390ddca8216cf47a5e90b1360
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-10-05 11:32:29 +0900

    WIP:文法上、推奨されていない処理を修正した

--- usingScripts/compile_all.sh
+++ usingScripts/compile_all.sh
@@ -5,16 +5,16 @@
 benchmarks=("bt" "cg" "dt" "ep" "ft" "is" "lu" "mg" "sp")
 classes=("S" "W" "A" "B" "C" "D" "E" "F")
 
-for benchmark in ${benchmarks[@]}
+for benchmark in "${benchmarks[@]}"
 do
-	for class in ${classes[@]}
+	for class in "${classes[@]}"
 	do
-		if [ ${benchmark} = "is" -a ${class} = "F" ]; then
-			echo "echo \""${benchmark}"に"${class}"はありません\""
-		elif [ ${benchmark} = "dt" ] && [ ${class} = "E" -o ${class} = "F" ]; then
-			echo ${benchmark}"には、"${class}"はありません"
+		if [ "${benchmark}" = "is" -a "${class}" = "F" ]; then
+			echo "echo \"${benchmark}に${class}はありません\""
+		elif [ "${benchmark}" = "dt" ] && [ "${class}" = "E" -o "${class}" = "F" ]; then
+			echo "echo \"${benchmark}には${class}はありません\""
 		else
-			echo "make "${benchmark}" CLASS="${class}
+			echo "make ${benchmark} CLASS=${class}"
 		fi
 	done
 done
