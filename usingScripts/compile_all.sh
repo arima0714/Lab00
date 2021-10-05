662367e33f5fb479e7b440b31554f3a05d9f4266
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-10-05 11:25:59 +0900

    ベンチマークプログラムをコンパイルするために用いるスクリプトを作成中

---
+++ usingScripts/compile_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 一気に全種類の実行ファイルをコンパイルするためのコマンド列を出力するスクリプト
+
+benchmarks=("bt" "cg" "dt" "ep" "ft" "is" "lu" "mg" "sp")
+classes=("S" "W" "A" "B" "C" "D" "E" "F")
+
+for benchmark in ${benchmarks[@]}
+do
+	for class in ${classes[@]}
+	do
+		if [ ${benchmark} = "is" -a ${class} = "F" ]; then
+			echo "echo \""${benchmark}"に"${class}"はありません\""
+		elif [ ${benchmark} = "dt" ] && [ ${class} = "E" -o ${class} = "F" ]; then
+			echo ${benchmark}"には、"${class}"はありません"
+		else
+			echo "make "${benchmark}" CLASS="${class}
+		fi
+	done
+done
