dfe2824d343a04e0767d71cb83ede880f9a6365f
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-10-21 13:59:48 +0900

    問題サイズ・コア数の抽出条件を２列に分割されたカラムで選択するようにした

--- app/app1.py
+++ app/app1.py
@@ -34,6 +34,73 @@ def app():
         processes=processes,
         csvDirPath=csvDirPath,
     )
+
+    st.markdown("## 抽出条件の選択")
+
+    ## 2列にして、コア数・問題サイズを選択する
+    ### 列の作成
+    columnForCore, columnForSize = st.columns(2)
+    ### コア数に関する列の設定
+    columnForCore.subheader("コア数")
+    #### 実際にデータ内にあるコア数のリストを作成
+
+    enable001 = columnForCore.checkbox("コア数1", value=True)
+    enable002 = columnForCore.checkbox("コア数2")
+    enable004 = columnForCore.checkbox("コア数4")
+    enable008 = columnForCore.checkbox("コア数8")
+    enable016 = columnForCore.checkbox("コア数16")
+    enable032 = columnForCore.checkbox("コア数32")
+    enable064 = columnForCore.checkbox("コア数64")
+    enable128 = columnForCore.checkbox("コア数128")
+    enable256 = columnForCore.checkbox("コア数256")
+
+    numOfCoreSet = set()
+    if enable001:
+        numOfCoreSet.add(1)
+    if enable002:
+        numOfCoreSet.add(2)
+    if enable004:
+        numOfCoreSet.add(4)
+    if enable008:
+        numOfCoreSet.add(8)
+    if enable016:
+        numOfCoreSet.add(16)
+    if enable032:
+        numOfCoreSet.add(32)
+    if enable064:
+        numOfCoreSet.add(64)
+    if enable128:
+        numOfCoreSet.add(128)
+    if enable256:
+        numOfCoreSet.add(256)
+
+    numOfCoreList = sorted(list(numOfCoreSet))
+    ### 問題サイズに関する列の設定
+    columnForSize.subheader("問題サイズ")
+    #### 実際にデータ内にある問題サイズのリストを作成
+
+    enableA = columnForSize.checkbox("問題サイズA", value=True)
+    enableB = columnForSize.checkbox("問題サイズB")
+    enableC = columnForSize.checkbox("問題サイズC")
+    enableD = columnForSize.checkbox("問題サイズD")
+    enableE = columnForSize.checkbox("問題サイズE")
+    enableF = columnForSize.checkbox("問題サイズF")
+
+    programsizeSet = set()
+    if enableA:
+        programsizeSet.add("A")
+    if enableB:
+        programsizeSet.add("B")
+    if enableC:
+        programsizeSet.add("C")
+    if enableD:
+        programsizeSet.add("D")
+    if enableE:
+        programsizeSet.add("E")
+    if enableF:
+        programsizeSet.add("F")
+    programsizeList = sorted(list(programsizeSet))
+
     # 問題サイズを数値化
     programSize = rawDataDF["benchmarkClass"].tolist()
     programSizeInNum = lib.convertBenchmarkClasses_problemSizeInNPB(
