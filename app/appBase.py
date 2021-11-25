6f52254bcb5756b85587f44dfcd8ec9ce74bd794
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-11-25 16:48:47 +0900

    WIP:作成中の部分のリンクを追加

--- app/appBase.py
+++ app/appBase.py
@@ -1,5 +1,6 @@
 import app1
 import app2
+import app3
 
 from multiapp import MultiApp
 
@@ -7,5 +8,6 @@ app = MultiApp()
 
 app.add_app("３次元および２次元プロット", app1.app)
 app.add_app("データの確認", app2.app)
+app.add_app("モデルのプロット", app3.app)
 
 app.run()
